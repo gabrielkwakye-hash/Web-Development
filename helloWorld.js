53f78019f187b56f013c8f044e7f0169fbf8c663
gabrielkwakye-hash <gabrielkwakye719@gmail.com>
2025-09-26 02:19:38 +0000

    Clucking Bell Website

---
+++ helloWorld.js
@@ -0,0 +1,11 @@
+let myVariable = 10;
+const myConstant = 5;
+var oldStyle = "hello";
+
+console.log(myVariable)
+
+console.log("I am " + myVariable + " years old.")
+
+console.log("We leave tomorrow at " + (myVariable + 2) + " o'clock.")
+
+console.log("Now I know how to create variables and print in the terminal.")
